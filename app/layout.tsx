1b335661050e2ec8a3bba6d14386db61ad760d0b
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2026-03-13 16:20:14 +0000

    chore: update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,12 +43,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
-          `}
+          {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${GA_TRACKING_ID}');`}
         </Script>
       </body>
     </html>
